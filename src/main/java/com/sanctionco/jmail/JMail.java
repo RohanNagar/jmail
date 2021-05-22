7eaa20d4419c5e32662008f8513505f37885b6e0
Rohan Nagar <rohan.nagar@utexas.edu>
2021-05-21 17:23:38 -0700

    Better character sets

--- src/main/java/com/sanctionco/jmail/JMail.java
+++ src/main/java/com/sanctionco/jmail/JMail.java
@@ -422,15 +422,12 @@ public class JMail {
   }
 
   // Set of characters that are not allowed in the local-part outside of quotes
-  private static final Set<Character> DISALLOWED_UNQUOTED_CHARACTERS = Arrays
-      .stream(new Character[]{
-          '\t', '(', ')', ',', ':', ';', '<', '>', '@', '[', ']', '"'
-      })
-      .collect(Collectors.toCollection(HashSet::new));
+  private static final Set<Character> DISALLOWED_UNQUOTED_CHARACTERS = new HashSet<>(
+      Arrays.asList('\t', '(', ')', ',', ':', ';', '<', '>', '@', '[', ']', '"'));
 
   // Set of characters that are allowed in the domain
-  private static final Set<Character> ALLOWED_DOMAIN_CHARACTERS = Arrays
-      .stream(new Character[]{
+  private static final Set<Character> ALLOWED_DOMAIN_CHARACTERS = new HashSet<>(
+      Arrays.asList(
           // A - Z
           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -440,14 +437,9 @@ public class JMail {
           // 0 - 9
           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
           // Hyphen and dot (also allow whitespace between parts)
-          '-', '.', ' '
-      })
-      .collect(Collectors.toCollection(HashSet::new));
+          '-', '.', ' '));
 
   // Set of characters within local-part quotes that require an escape
-  private static final Set<Character> ALLOWED_QUOTED_WITH_ESCAPE = Arrays
-      .stream(new Character[]{
-          '\r', '␀', '\n'
-      })
-      .collect(Collectors.toCollection(HashSet::new));
+  private static final Set<Character> ALLOWED_QUOTED_WITH_ESCAPE = new HashSet<>(
+      Arrays.asList('\r', '␀', '\n'));
 }
