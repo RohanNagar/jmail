5381466541d713e74b859c5e5463f6173bb9c8c8
Ethan McCue <ethan.mccue@lumanu.com>
2021-05-22 20:56:01 -0400

    Don't use an enum for TLD

--- src/main/java/com/sanctionco/jmail/TopLevelDomain.java
+++ src/main/java/com/sanctionco/jmail/TopLevelDomain.java
@@ -1,6 +1,8 @@
 package com.sanctionco.jmail;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,26 +10,24 @@ import java.util.stream.Stream;
 /**
  * Enumerates known common top level domains.
  */
-public enum TopLevelDomain {
+public final class TopLevelDomain {
   // Original TLDs
   // https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains#Original_top-level_domains
-  DOT_COM("com"),
-  DOT_ORG("org"),
-  DOT_NET("net"),
-  DOT_INT("int"),
-  DOT_EDU("edu"),
-  DOT_GOV("gov"),
-  DOT_MIL("mil"),
+  public static final TopLevelDomain DOT_COM = new TopLevelDomain("com");
+  public static final TopLevelDomain DOT_ORG = new TopLevelDomain("org");
+  public static final TopLevelDomain DOT_NET = new TopLevelDomain("int");
+  public static final TopLevelDomain DOT_INT = new TopLevelDomain("edu");
+  public static final TopLevelDomain DOT_EDU = new TopLevelDomain("com");
+  public static final TopLevelDomain DOT_GOV = new TopLevelDomain("gov");
+  public static final TopLevelDomain DOT_MIL = new TopLevelDomain("mil");
 
   // To use when an email address does not have a top level domain
-  NONE(""),
 
-  // To use when the TLD is unknown
-  OTHER(" ");
+  public static final TopLevelDomain NONE = new TopLevelDomain("");
 
   private final String tld;
 
-  TopLevelDomain(String tld) {
+  private TopLevelDomain(String tld) {
     this.tld = tld;
   }
 
@@ -35,19 +35,37 @@ public enum TopLevelDomain {
     return tld;
   }
 
-  private static final Map<String, TopLevelDomain> MAP = Stream
-      .of(TopLevelDomain.values())
-      .collect(Collectors.toMap(TopLevelDomain::stringValue, UnaryOperator.identity()));
-
   /**
    * Get the {@code TopLevelDomain} that represents the given string.
    *
    * @param tld the string to turn into a {@code TopLevelDomain}
-   * @return the {@code TopLevelDomain}, or {@code TopLevelDomain.OTHER} if it is unknown
+   * @return a {@code TopLevelDomain}, or {@code TopLevelDomain.NONE} if null or empty TLD is given.
    */
   public static TopLevelDomain fromString(String tld) {
     if (tld == null || tld.isEmpty()) return NONE;
 
-    return MAP.getOrDefault(tld, OTHER);
+    return new TopLevelDomain(tld);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof TopLevelDomain)) return false;
+    TopLevelDomain that = (TopLevelDomain) o;
+    return tld.equals(that.tld);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(tld);
+  }
+
+  @Override
+  public String toString() {
+    return "TopLevelDomain["
+            + "tld='"
+            + tld
+            + '\''
+            + ']';
   }
 }
