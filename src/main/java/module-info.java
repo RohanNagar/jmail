265ed22ec241ffcb1e780d17f1c63bfa2f132a0d
Rohan Nagar <rohan.nagar@utexas.edu>
2021-05-24 18:37:44 -0700

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * A modern and lightweight library for working with email addresses in Java.
+ *
+ * @author Rohan Nagar (rohannagar11@gmail.com)
+ */
+open module com.sanctionco.jmail {
+  exports com.sanctionco.jmail;
+  exports com.sanctionco.jmail.net;
+}
