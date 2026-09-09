da885505a6288e824349b440e319a8311c4eda41
Rohan Nagar <rohannagar11@gmail.com>
2026-09-08 21:54:22 -0700

    test: improve EncodedWord test coverage

--- src/test/java/com/sanctionco/jmail/mime/EncodedWordTest.java
+++ src/test/java/com/sanctionco/jmail/mime/EncodedWordTest.java
@@ -13,6 +13,7 @@ class EncodedWordTest {
 
   @ParameterizedTest
   @MethodSource("encodedWordProvider")
+  @MethodSource("malformedWordProvider")
   void canDecodeWords(String word, String expected) {
     assertThat(EncodedWord.decodeText(word)).isEqualTo(expected);
   }
@@ -22,6 +23,18 @@ class EncodedWordTest {
     String word = "=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?=\n"
         + "  =?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=";
     String expected = "If you can read this you understand the example.";
+
+    assertThat(EncodedWord.decodeText(word)).isEqualTo(expected);
+
+    word = "=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?=\t"
+        + "=?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=";
+    expected = "If you can read this you understand the example.";
+
+    assertThat(EncodedWord.decodeText(word)).isEqualTo(expected);
+
+    word = "=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?=\r"
+        + "=?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=";
+    expected = "If you can read this you understand the example.";
 
     assertThat(EncodedWord.decodeText(word)).isEqualTo(expected);
   }
@@ -34,9 +47,20 @@ class EncodedWordTest {
         Arguments.of("=?utf-8?Q?Andreas_Birkeb=C3=A6k?=", "Andreas Birkebæk"),
         Arguments.of("=?utf-8?B?U3VqZXRpcyDEl3RpcyDFs3bEl25pcw==?=", "Sujetis ėtis ųvėnis"),
         Arguments.of("Unencoded word here", "Unencoded word here"),
-        Arguments.of(null, null),
         Arguments.of("=?utf-8?q?te?xt?=", "te?xt"),
         Arguments.of("=?utf-8?q?=40evil.com=00?=", "@evil.com\0")
+    );
+  }
+
+  List<Arguments> malformedWordProvider() {
+    return List.of(
+        Arguments.of(null, null),
+        Arguments.of("", ""),
+        Arguments.of("=?utf-8hello", "=?utf-8hello"),
+        Arguments.of("=?utf-8?bhello", "=?utf-8?bhello"),
+        Arguments.of("=?utf-8?b?hello", "=?utf-8?b?hello"),
+        Arguments.of("=?utf-8?z?te?xt?=", "=?utf-8?z?te?xt?="),
+        Arguments.of("=?utf-8?Q?Andreas_Birkeb=G1=1G=A?=", "Andreas Birkeb=G1=1G=A")
     );
   }
 }
